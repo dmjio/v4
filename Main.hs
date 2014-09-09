4003fc461b1979e045431eae4d455e626c59f4f5
David Johnson <djohnson.m@gmail.com>
2014-09-08 22:59:46 -0500

    updated v4

--- Main.hs
+++ Main.hs
@@ -1,5 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
+{-# LANGUAGE RecordWildCards   #-}
+
+module V4 
+    ( -- * Types
+      Method           (..)  
+    , CanonicalRequest (..)
+    , SecretKey        (..)
+    , Region
+    , Service
+    , Params
+      -- * API Functions
+    , createSignature
+    )
+    where
 
 import           Control.Applicative
 import           Crypto.Hash            (Digest)
@@ -9,45 +22,129 @@ import           Data.ByteString        (ByteString)
 import           Data.ByteString.Base16 (encode)
 import qualified Data.ByteString.Char8  as B8
 import           Data.Char
+import           Data.String
 import           Data.Monoid
 import           Data.Time
+import           Data.Function
+import           Data.List
+
 import           System.IO.Unsafe
 import           System.Locale
 
-main :: IO ()
-main = putStrLn "testing aws..."
+type Region  = ByteString
+type Service = ByteString
+type Params  = [(ByteString, ByteString)]
 
--- | Step 1: create canonical request
-req :: ByteString
-req = B8.intercalate "\n" [ method
+newtype SecretKey = 
+    SecretKey ByteString deriving (Eq, Show)
+
+data Method = 
+    GET 
+  | PUT
+  | POST
+  | DELETE
+    deriving (Show, Eq)
+
+data CanonicalRequest = CanonicalRequest {
+      canonicalRequestMethod  :: Method
+    , canonicalURI            :: ByteString
+    , canonicalQueryString    :: ByteString
+    , canonicalHeaders        :: Params
+    , canonicalSignedHeaders  :: [ByteString]
+    , canonicalRequestActions :: Params
+} deriving (Show, Eq)
+
+secret :: ByteString
+secret = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY"
+
+createSignature
+    :: SecretKey        -- ^ Your AWS Secret Key
+    -> CanonicalRequest -- ^ The Canonical Request 
+    -> Region           -- ^ The region you wish to use to perform this AWS operation
+    -> Service          -- ^ The service you'd like to use to perfrom this AWS operation
+    -> IO ByteString
+createSignature
+    secretKey
+    canonicalRequest
+    region
+    service
+    = encode <$> liftA2 hmacSHA256 key string
+  where key    = signingKey secretKey region service
+        string = stringToSign canonicalRequest region service
+
+stringToSign
+    :: CanonicalRequest
+    -> Region
+    -> Service
+    -> IO ByteString
+stringToSign
+    canonicalRequest
+    region
+    service = do
+      smallDate <- dateTime
+      longDate  <- isoDate
+      let credScope = B8.intercalate "/" [
+                        smallDate
+                      , region
+                      , service
+                      , "aws4_request"
+                      ]
+      return $ B8.intercalate "\n" [
+                  "AWS4-HMAC-SHA256"
+                 , "20110909T233600Z"
+                 , credScope
+                 , hexEncode $ createCanonical canonicalRequest
+                 ]
+signingKey
+    :: SecretKey
+    -> Region
+    -> Service
+    -> IO ByteString
+signingKey
+  (SecretKey secretKey)
+  region
+  service = do
+    date <- dateTime
+    return $ foldr hmacSHA256 ("AWS4" <> secretKey) [
+                 date
+               , region
+               , service
+               , "aws4_request"
+               ]
+
+createCanonical
+    :: CanonicalRequest
+    -> ByteString
+createCanonical 
+    CanonicalRequest {..}
+    = B8.intercalate "\n" [ method
                           , uri
-                          , queryString
+                          , queryString 
                           , headers
                           , signedHeaders
-                          , hexEncode (hash hexHashPayload)
+                          , hexEncode requestActions
                           ]
-  where method         = "POST"
-        uri            = "/"
-        queryString    = ""
-        headers        = "content-type:application/x-www-form-urlencoded; charset=utf-8\nhost:iam.amazonaws.com\nx-amz-date:20110909T233600Z\n"
-        signedHeaders  = "content-type;host;x-amz-date"
-        hexHashPayload = "Action=ListUsers&Version=2010-05-08"
+  where method         = B8.pack $ show canonicalRequestMethod
+        uri            = canonicalURI
+        queryString    = canonicalQueryString
+        headers        = foldr (\(k,v) c -> 
+                                    B8.concat [ 
+                                           B8.map toLower k 
+                                          , ":", v, "\n", c 
+                                          ]) mempty (sortBy (compare `on` fst) canonicalHeaders)
+        signedHeaders  = B8.intercalate ";" canonicalSignedHeaders
+        requestActions = toQueryString canonicalRequestActions
 
-hmacSHA256 :: ByteString -> ByteString -> ByteString
-hmacSHA256 secret msg = hmac hash 64 secret msg
+hmacSHA256
+    :: ByteString
+    -> ByteString
+    -> ByteString
+hmacSHA256 = hmac hash 64
 
-lower :: ByteString -> ByteString
-lower = B8.map toLower
-
-hexEncode :: ByteString -> ByteString
-hexEncode = lower . encode
-
-removeWhiteSpace :: ByteString -> ByteString
-removeWhiteSpace = B8.filter (/=' ')
-
--- Step 2: hash canonical
-hashedCanonical :: ByteString
-hashedCanonical = hexEncode (hash req)
+hexEncode
+    :: ByteString
+    -> ByteString
+hexEncode = B8.map toLower . encode . hash
 
 dateTime :: IO ByteString
 dateTime = fmt <$> getCurrentTime
@@ -57,33 +154,8 @@ isoDate :: IO ByteString
 isoDate = iso8601 <$> getCurrentTime
   where iso8601 = B8.pack . formatTime defaultTimeLocale "%Y%m%dT%H%M%SZ"
 
--- Step 3: Create string to sign
-stringToSign :: ByteString
-stringToSign = B8.intercalate "\n" [ algo
-                                   , reqDate
-                                   , credScope
-                                   , hashedCanonicalReq
-                                   ]
-  where algo               = "AWS4-HMAC-SHA256"
-        reqDate            = "20110909T233600Z"
-        credScope          = B8.intercalate "/" [ "20110909"
-                                                , "us-east-1"
-                                                , "iam"
-                                                , "aws4_request"
-                                                ]
-        hashedCanonicalReq = hashedCanonical
-
-secret :: ByteString
-secret = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY"
-
-signingKey :: ByteString
-signingKey = kSigning
-  where kDate    = hmacSHA256 ("AWS4" <> secret) "20110909"
-        kRegion  = hmacSHA256 kDate    "us-east-1"
-        kService = hmacSHA256 kRegion  "iam"
-        kSigning = hmacSHA256 kService "aws4_request"
-
-signature :: ByteString
-signature = encode $ hmacSHA256 signingKey stringToSign
-
+toQueryString :: [(ByteString, ByteString)] -> ByteString
+toQueryString [] = ""
+toQueryString ((x,y):[]) = x <> "=" <> y
+toQueryString ((x,y):xs) = x <> "=" <> y <> "&" <> toQueryString xs
 
